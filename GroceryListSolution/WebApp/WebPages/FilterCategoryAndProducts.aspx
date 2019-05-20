d9e11fb3f0c336ef8d53a50f323246d336ac22cf
Rranawak1 <43081663+Rranawak1@users.noreply.github.com>
2019-05-19 19:27:17 -0600

    Created the First Web Page
    Made the first ODS but it's not finished

---
+++ GroceryListSolution/WebApp/WebPages/FilterCategoryAndProducts.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="Filtering Products" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FilterCategoryAndProducts.aspx.cs" Inherits="WebApp.WebPages.FilterCategoryAndProducts" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Query: ODS</h1>
+
+    <div class="col-md-offset-3">
+    <asp:Label ID="label1" runat="server" Text="Select a Category "></asp:Label>
+    &nbsp;&nbsp;
+    <asp:ObjectDataSource ID="CategoryListODS" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="Category_List" 
+                              TypeName="GroceryListSystem.BLL.CategoryController"></asp:ObjectDataSource>
+    <asp:DropDownList ID="CategoryList" runat="server"></asp:DropDownList>
+    &nbsp;&nbsp;
+    <asp:LinkButton ID="FetchButton" runat="server" CausesValidation="false">Fetch</asp:LinkButton>
+    <br /><br />
+
+        <asp:ObjectDataSource ID="ProductGridViewListODS" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="Category_GetByProduct" TypeName="GroceryListSystem.BLL.ProductController">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="CategoryList" PropertyName="SelectedValue" Name="categoryid" Type="Int32"></asp:ControlParameter>
+            </SelectParameters>
+        </asp:ObjectDataSource>
+        <asp:GridView ID="ProductList" runat="server" AutoGenerateColumns="False" DataSourceID="ProductGridViewListODS" >
+            <Columns>
+                <asp:BoundField DataField="ProductID" HeaderText="ProductID" SortExpression="ProductID"></asp:BoundField>
+                <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description"></asp:BoundField>
+                <asp:BoundField DataField="Price" HeaderText="Price" SortExpression="Price"></asp:BoundField>
+                <asp:BoundField DataField="Discount" HeaderText="Discount" SortExpression="Discount"></asp:BoundField>
+                <asp:BoundField DataField="UnitSize" HeaderText="UnitSize" SortExpression="UnitSize"></asp:BoundField>
+                <asp:CheckBoxField DataField="Taxable" HeaderText="Taxable" SortExpression="Taxable"></asp:CheckBoxField>
+            </Columns>
+        </asp:GridView>
+
+        
+
+    </div>
+</asp:Content>
---
+++ GroceryListSolution/WebApp/WebPages/FilterCategoryAndProducts.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using GroceryListSystem.BLL;
+using GroceryList.Data.Entities;
+#endregion
+
+namespace WebApp.WebPages
+{
+    public partial class FilterCategoryAndProducts : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindCategory();
+            }
+        }
+
+        protected void BindCategory()
+        {
+            CategoryController sysmgr = new CategoryController();
+            List<Category> info = sysmgr.Category_List();
+            info.Sort((x, y) => x.Description.CompareTo(y.Description));
+            CategoryList.DataSource = info;
+            CategoryList.DataTextField = nameof(Category.Description);
+            CategoryList.DataValueField = nameof(Category.CategoryID);
+            CategoryList.DataBind();
+        }
+
+        protected void ProductList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow agrow = ProductList.Rows[ProductList.SelectedIndex];
+            string productid = (agrow.FindControl("ProductID") as Label).Text;
+        }
+
+        //protected void AlbumList_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    //access data located on the gridviewrow
+        //    //template web control access syntax
+        //    GridViewRow agvrow = AlbumList.Rows[AlbumList.SelectedIndex]; // the row is selected that's pointed at
+        //    string albumid = (agvrow.FindControl("AlbumID") as Label).Text; // will allow me to find any field in that row even though the data is hidden
+        //                                                                    //albumid = "0"; //testing to cause an error REMOVE WHEN Finished testing
+
+        //    MessageUserControl.TryRun(() => {
+        //        //standard lookup
+        //        AlbumController sysmgr = new AlbumController();
+        //        Album datainfo = sysmgr.Album_Get(int.Parse(albumid));
+        //        if (datainfo == null) //to check if there's items in the list use .Count
+        //        {
+        //            ClearControls(); // only activated if this part of the if statement is valid
+        //            throw new Exception("No records found for selected album. Refresh your list of albums");
+        //        }
+        //        else
+        //        {
+        //            EditAlbumID.Text = datainfo.AlbumId.ToString();
+        //            EditTitle.Text = datainfo.Title;
+        //            EditAlbumArtistList.SelectedValue = datainfo.ArtistId.ToString();
+        //            EditReleaseYear.Text = datainfo.ReleaseYear.ToString();
+        //            EditReleaseLabel.Text = datainfo.ReleaseLabel == null ? "" : datainfo.ReleaseLabel;
+        //        }
+
+        //    }, "Album Search", "View Album Details");
+
+        //}
+    }
+}
