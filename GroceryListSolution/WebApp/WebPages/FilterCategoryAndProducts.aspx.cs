df0c8bb33f55a8804c2933f70236827433eadf6c
Rranawak1 <43081663+Rranawak1@users.noreply.github.com>
2019-05-22 18:11:15 -0600

    MessageUserControl is added to the cs page of the webpage

--- GroceryListSolution/WebApp/WebPages/FilterCategoryAndProducts.aspx.cs
+++ GroceryListSolution/WebApp/WebPages/FilterCategoryAndProducts.aspx.cs
@@ -24,13 +24,17 @@ namespace WebApp.WebPages
 
         protected void BindCategory()
         {
-            CategoryController sysmgr = new CategoryController();
-            List<Category> info = sysmgr.Category_List();
-            info.Sort((x, y) => x.Description.CompareTo(y.Description));
-            CategoryList.DataSource = info;
-            CategoryList.DataTextField = nameof(Category.Description);
-            CategoryList.DataValueField = nameof(Category.CategoryID);
-            CategoryList.DataBind();
+			MessageUserControl.TryRun(() =>
+			{
+				CategoryController sysmgr = new CategoryController();
+				List<Category> info = sysmgr.Category_List();
+				info.Sort((x, y) => x.Description.CompareTo(y.Description));
+				CategoryList.DataSource = info;
+				CategoryList.DataTextField = nameof(Category.Description);
+				CategoryList.DataValueField = nameof(Category.CategoryID);
+				CategoryList.DataBind();
+			});
+            
         }
 
         protected void ProductList_SelectedIndexChanged(object sender, EventArgs e)
@@ -38,35 +42,5 @@ namespace WebApp.WebPages
             GridViewRow agrow = ProductList.Rows[ProductList.SelectedIndex];
             string productid = (agrow.FindControl("ProductID") as Label).Text;
         }
-
-        //protected void AlbumList_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-        //    //access data located on the gridviewrow
-        //    //template web control access syntax
-        //    GridViewRow agvrow = AlbumList.Rows[AlbumList.SelectedIndex]; // the row is selected that's pointed at
-        //    string albumid = (agvrow.FindControl("AlbumID") as Label).Text; // will allow me to find any field in that row even though the data is hidden
-        //                                                                    //albumid = "0"; //testing to cause an error REMOVE WHEN Finished testing
-
-        //    MessageUserControl.TryRun(() => {
-        //        //standard lookup
-        //        AlbumController sysmgr = new AlbumController();
-        //        Album datainfo = sysmgr.Album_Get(int.Parse(albumid));
-        //        if (datainfo == null) //to check if there's items in the list use .Count
-        //        {
-        //            ClearControls(); // only activated if this part of the if statement is valid
-        //            throw new Exception("No records found for selected album. Refresh your list of albums");
-        //        }
-        //        else
-        //        {
-        //            EditAlbumID.Text = datainfo.AlbumId.ToString();
-        //            EditTitle.Text = datainfo.Title;
-        //            EditAlbumArtistList.SelectedValue = datainfo.ArtistId.ToString();
-        //            EditReleaseYear.Text = datainfo.ReleaseYear.ToString();
-        //            EditReleaseLabel.Text = datainfo.ReleaseLabel == null ? "" : datainfo.ReleaseLabel;
-        //        }
-
-        //    }, "Album Search", "View Album Details");
-
-        //}
     }
 }
